046d62009c2f8e7c207cccbbbf8b27efab183e2c
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2022-05-03 13:50:18 +0100

    Csp 74 reload standards cache (#3)
    * CSP-74 saving interim work as need to rework configuration
    
    * CSP-74 preliminary work - getting courses for courses-api, posting courses to roatp-api
    
    * CSP-74 coding committed, incomplete
    
    * CSP-74 reworking code to use httpStatusCode
    
    * CSP-74 coverage complete
    
    * CSP-74 coverage and code snagging, now complete
    
    * CSP-74 reworked folders and removed code smells
    
    * CSP-74 fixed single code smell
    
    * CSP-74 remedy single code smell break
    
    * CSP-74 reworked folders and apiclient names
    
    * CSP-74 reworking based on TR
    
    * CSP-74 removed unneeded call in ApiClientBase and added protective code around reponse failure
    
    * CSP-74 removing startup from coverage
    
    * CSP-74 added check for non response from courses-api
    
    * CSP-74 Included coverlet for coverage
    
    * CSP-74 excluded config from code coverage
    
    * CSP-74 reversing unintended commit and adding coverage exclusion
    
    Co-authored-by: Deven Shah <devenshah1974@gmail.com>

---
+++ src/SFA.DAS.Roatp.CourseManagement.Jobs/Infrastructure/ApiClients/RoatpV2Api/Models/StandardsRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.Roatp.CourseManagement.Jobs.Infrastructure.ApiClients.StandardsApi.Models;
+
+namespace SFA.DAS.Roatp.CourseManagement.Jobs.Infrastructure.ApiClients.RoatpV2Api.Models
+{
+    public class StandardsRequest
+    {
+        public List<Standard> Standards { get; set; }
+    }
+}
